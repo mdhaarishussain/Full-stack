50229d8b19ca209d280921e592ee9f11d6209787
mdhaarishussain <mdhaarishussain@gmail.com>
2024-11-25 10:27:51 +0530

    added day 8 files

---
+++ Session-8/API/script.js
@@ -0,0 +1,3 @@
+//what are promises
+//how are they handled
+//in memory game , randomness, event handling nodes,
---
+++ Session-8/MEMORY/script.js
@@ -0,0 +1,10 @@
+//<Array Methods>
+//<DOM Manipulation>
+//<Event Handling>
+//<Function Declaration>
+//<Conditional Statements>
+//<setTimeout Method>
+//<Data Attributes>
+const cardValues=['A','A','B','B','C','C','D','D']
+const shuffleCards= cardValues.sort(()=>0.5-Math.random());
+console.log(shuffleCards);
