6c28b7f691fa1fe66c99a770bc04dd5378aeafa2
mdhaarishussain <mdhaarishussain@gmail.com>
2024-12-02 11:57:14 +0530

    medille commit

--- Node/file.js
+++ Node/file.js
@@ -8,15 +8,27 @@ const fs=require("fs")
       
 // });
 
-fs.readFileSync("./rfile.txt","utf-8",(err,result)=>{
-    if(err){
-        console.log("Error:",err);
-    }
-    else{
-        console.log("Result",result);
+// fs.readFileSync("./rfile.txt","utf-8",(err,result)=>{
+//     if(err){
+//         console.log("Error:",err);
+//     }
+//     else{
+//         console.log("Result",result);
 
         
-    }
-});
+//     }
+// });
 
-fs.appendFileSync("./wfile.txt",`${Math.random()}"new lines from append\n`);
+// fs.appendFileSync("./wfile.txt",`${Math.random()}"new lines from append\n`);
+// fs.cpSync("./wfile.txt","./wcopy.txt");
+// fs.unlinkSync("./wcopy.txt");
+// console.log(fs.statSync("./file.js"));
+// fs.mkdirSync('dircode/a/b',{recursive:true});
+
+// const os=require("os");
+// console.log(os);
+// console.log(os.cpus().lenght);
+// console.log(os.totalmem());
+
+console.log("Before Sync");
+const result=fs.read
